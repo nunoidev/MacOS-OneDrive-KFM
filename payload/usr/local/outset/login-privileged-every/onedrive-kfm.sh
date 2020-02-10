9708b9fbcd01834837d5bc6b4c3a404db82d301f
nunoidev <nuno.carvalheiro87@outlook.com>
2020-02-10 23:55:52 +1100

    Added ability to sync other folders
    Added option to read from preferences for choice of folder sync, and coding for performing the sync.

--- payload/usr/local/outset/login-privileged-every/onedrive-kfm.sh
+++ payload/usr/local/outset/login-privileged-every/onedrive-kfm.sh
@@ -16,6 +16,15 @@ defaultOneDriveName=$(/usr/bin/python -c "from Foundation import CFPreferencesCo
 oneDriveKFMEnabled=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('EnableKFM', 'com.cambridgeconsultants.onedrive-kfm')")
 fixBadFileNames=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('FixBadFileNames', 'com.cambridgeconsultants.onedrive-kfm')")
 
+#Get folders choice to sync
+syncDesktopFolder=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('syncDesktopFolder', 'com.cambridgeconsultants.onedrive-kfm')")
+syncDocumentsFolder=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('syncDocumentsFolder', 'com.cambridgeconsultants.onedrive-kfm')")
+syncPicturesFolder=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('syncPicturesFolder', 'com.cambridgeconsultants.onedrive-kfm')")
+syncMoviesFolder=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('syncMoviesFolder', 'com.cambridgeconsultants.onedrive-kfm')")
+syncMusicFolder=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('syncMusicFolder', 'com.cambridgeconsultants.onedrive-kfm')")
+syncDownloadsFolder=$(/usr/bin/python -c "from Foundation import CFPreferencesCopyAppValue; print CFPreferencesCopyAppValue('syncDownloadsFolder', 'com.cambridgeconsultants.onedrive-kfm')")
+
+
 #Only enable OneDrive KFM for machines with a preference file turning it on
 if [ "$oneDriveKFMEnabled" != "True" ] && [ "$defaultTenantID" == "None" ] && [ "$defaultOneDriveName" == "None" ]; then
     logger -s -p user.error "OneDrive-KFM: Not enabled on this computer.  To enable ensure that EnableKFM, FixBadFileNames, TenantID, and OneDriveFolderName are set within the /Library/Preferences/com.cambridgeconsultants.onedrive-kfm.plist file."
@@ -77,28 +86,88 @@ for user in $userList; do
             logger -s -p user.notice "OneDrive-KFM: creating backup path: $backupPath"
             mkdir -p "$backupPath"
 
-            if [ -d "$userHomeDirectory/Documents" ] && [ ! -L "$userHomeDirectory/Documents" ]; then
-                logger -s -p user.notice "OneDrive-KFM: Moving documents folder for $user."
-                mv -f "$userHomeDirectory/Documents" "$backupPath/Documents"
+            if [ "$syncDesktopFolder" == "True" ]; then
+                if [ -d "$userHomeDirectory/Documents" ] && [ ! -L "$userHomeDirectory/Documents" ]; then
+                    logger -s -p user.notice "OneDrive-KFM: Moving documents folder for $user."
+                    mv -f "$userHomeDirectory/Documents" "$backupPath/Documents"
 
-                logger -s -p user.notice "OneDrive-KFM: Building symlink for documents folder for $user."
-                mkdir -p "$defaultOneDriveFolder/Documents"
-                ln -s "$defaultOneDriveFolder/Documents" "$userHomeDirectory/Documents"
-                /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Documents" >/dev/null 2>&1
-            else
-                logger -s -p user.error "OneDrive-KFM: Documents folder not moved or already moved for $user"
+                    logger -s -p user.notice "OneDrive-KFM: Building symlink for documents folder for $user."
+                    mkdir -p "$defaultOneDriveFolder/Documents"
+                    ln -s "$defaultOneDriveFolder/Documents" "$userHomeDirectory/Documents"
+                    /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Documents" >/dev/null 2>&1
+                else
+                    logger -s -p user.error "OneDrive-KFM: Documents folder not moved or already moved for $user"
+                fi
             fi
 
-            if [ -d "$userHomeDirectory/Desktop" ] && [ ! -L "$userHomeDirectory/Desktop" ]; then
-                logger -s -p user.notice "OneDrive-KFM: Moving desktop folder for $user."
-                mv -f "$userHomeDirectory/Desktop" "$backupPath/Desktop"
+            if [ "$syncDocumentsFolder" == "True" ]; then
+                if [ -d "$userHomeDirectory/Desktop" ] && [ ! -L "$userHomeDirectory/Desktop" ]; then
+                    logger -s -p user.notice "OneDrive-KFM: Moving desktop folder for $user."
+                    mv -f "$userHomeDirectory/Desktop" "$backupPath/Desktop"
 
-                logger -s -p user.notice "OneDrive-KFM: Building symlink for desktop folder for $user."
-                mkdir -p "$defaultOneDriveFolder/Desktop"
-                ln -s "$defaultOneDriveFolder/Desktop" "$userHomeDirectory/Desktop"
-                /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Desktop" >/dev/null 2>&1
-            else
-                logger -s -p user.error "OneDrive-KFM: Desktop folder not moved or already moved for $user"
+                    logger -s -p user.notice "OneDrive-KFM: Building symlink for desktop folder for $user."
+                    mkdir -p "$defaultOneDriveFolder/Desktop"
+                    ln -s "$defaultOneDriveFolder/Desktop" "$userHomeDirectory/Desktop"
+                    /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Desktop" >/dev/null 2>&1
+                else
+                    logger -s -p user.error "OneDrive-KFM: Desktop folder not moved or already moved for $user"
+                fi
+            fi
+            
+            if [ "$syncPicturesFolder" == "True" ]; then
+                if [ -d "$userHomeDirectory/Pictures" ] && [ ! -L "$userHomeDirectory/Pictures" ]; then
+                    logger -s -p user.notice "OneDrive-KFM: Moving Pictures folder for $user."
+                    mv -f "$userHomeDirectory/Pictures" "$backupPath/Pictures"
+
+                    logger -s -p user.notice "OneDrive-KFM: Building symlink for Pictures folder for $user."
+                    mkdir -p "$defaultOneDriveFolder/Pictures"
+                    ln -s "$defaultOneDriveFolder/Pictures" "$userHomeDirectory/Pictures"
+                    /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Pictures" >/dev/null 2>&1
+                else
+                    logger -s -p user.error "OneDrive-KFM: Pictures folder not moved or already moved for $user"
+                fi  
+            fi
+            
+            if [ "$syncMoviesFolder" == "True" ]; then
+                if [ -d "$userHomeDirectory/Movies" ] && [ ! -L "$userHomeDirectory/Movies" ]; then
+                    logger -s -p user.notice "OneDrive-KFM: Moving Movies folder for $user."
+                    mv -f "$userHomeDirectory/Movies" "$backupPath/Movies"
+
+                    logger -s -p user.notice "OneDrive-KFM: Building symlink for Movies folder for $user."
+                    mkdir -p "$defaultOneDriveFolder/Movies"
+                    ln -s "$defaultOneDriveFolder/Movies" "$userHomeDirectory/Movies"
+                    /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Movies" >/dev/null 2>&1
+                else
+                    logger -s -p user.error "OneDrive-KFM: Movies folder not moved or already moved for $user"
+                fi  
+            fi
+            
+            if [ "$syncMusicFolder" == "True" ]; then
+                if [ -d "$userHomeDirectory/Music" ] && [ ! -L "$userHomeDirectory/Music" ]; then
+                    logger -s -p user.notice "OneDrive-KFM: Moving Music folder for $user."
+                    mv -f "$userHomeDirectory/Music" "$backupPath/Music"
+
+                    logger -s -p user.notice "OneDrive-KFM: Building symlink for Music folder for $user."
+                    mkdir -p "$defaultOneDriveFolder/Music"
+                    ln -s "$defaultOneDriveFolder/Music" "$userHomeDirectory/Music"
+                    /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Music" >/dev/null 2>&1
+                else
+                    logger -s -p user.error "OneDrive-KFM: Music folder not moved or already moved for $user"
+                fi  
+            fi
+            
+             if [ "$syncDownloadsFolder" == "True" ]; then
+                if [ -d "$userHomeDirectory/Downloads" ] && [ ! -L "$userHomeDirectory/Downloads" ]; then
+                    logger -s -p user.notice "OneDrive-KFM: Moving Downloads folder for $user."
+                    mv -f "$userHomeDirectory/Downloads" "$backupPath/Downloads"
+
+                    logger -s -p user.notice "OneDrive-KFM: Building symlink for Downloads folder for $user."
+                    mkdir -p "$defaultOneDriveFolder/Downloads"
+                    ln -s "$defaultOneDriveFolder/Downloads" "$userHomeDirectory/Downloads"
+                    /usr/sbin/chown -Rv "$user" "$defaultOneDriveFolder/Downloads" >/dev/null 2>&1
+                else
+                    logger -s -p user.error "OneDrive-KFM: Downloads folder not moved or already moved for $user"
+                fi  
             fi
 
             logger -s -p user.notice "OneDrive-KFM: Ensuring ownership of backup data set to $user."
